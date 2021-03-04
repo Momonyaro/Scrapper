475cae852bb7e69b7cbf8e0097bb53a7f5f3d2e9
Momonyaro <sebastian.alkstrand@gmail.com>
2021-03-04 22:50:45 +0100

    Added the dialogue UI along with animations for it.
    Om du klickar på --DIALOGUE PANEL-- så kan du toggle:a showMenu för att spela upp animationerna.

---
+++ Assets/Scripts/UI/DialogueMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueMenu : MonoBehaviour
+{
+    public bool showMenu = false;
+    public Animator animator;
+    private bool _lastState = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_lastState != showMenu)
+        {
+            animator.SetBool("SHOW_MENU", showMenu);
+            _lastState = showMenu;
+        }
+    }
+}
